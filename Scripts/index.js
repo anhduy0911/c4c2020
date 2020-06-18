9f58ab8d900def7c8b08a8e7f3990f1095581e0d
Anh <anhduy0911@gmail.com>
2020-06-19 06:10:04 +0700

    train model

--- Scripts/index.js
+++ Scripts/index.js
@@ -1,6 +1,43 @@
 var app = new Vue({
 	el: '#app',
-	data: {
-		name: 'Duy',
+	data: function () {
+		//TODO: Call to API here to get input list and output list
+		return {
+			input_list: [{
+				field: 'A'
+			}, {
+				field: 'B'
+			}, {
+				field: 'C'
+			}, {
+				field: 'D'
+			}, {
+				field: 'E'
+			}, {
+				field: 'F'
+			}, {
+				field: 'G'
+			}, {
+				field: 'H'
+			}],
+			output_list: [{
+				field: 'A'
+			}, {
+				field: 'B'
+			}, {
+				field: 'C'
+			}, {
+				field: 'D'
+			}, {
+				field: 'E'
+			}, {
+				field: 'F'
+			}, {
+				field: 'G'
+			}, {
+				field: 'H'
+			}],
+			time_frame: ['Day', 'Month', 'Year']
+		}
 	},
 });
